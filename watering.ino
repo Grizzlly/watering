17f1f7f6fe1b86570b08f91a557f8183744591a2
Andrei Stan <standrei2003@yahoo.com>
2024-01-17 18:02:58 +0200

    Changed pin and max pump time.

--- watering.ino
+++ watering.ino
@@ -1,50 +1,85 @@
 
-#define SOIL_PIN A0
+#define SOIL_PIN A5
 
-static const int VERY_WET = 350;
+static const int VERY_WET = 290;
 static const int VERY_DRY = 670;
 static const int AVG = (VERY_WET + VERY_DRY) / 2;
 
-static const int MIN_PUMP_TIME = 1000;
-static const int MAX_PUMP_TIME = 5000;
+static const int MIN_PUMP_TIME = 2000;
+static const int MAX_PUMP_TIME = 7000;
 
 static unsigned int pumpStartMilis = 0;
 static bool pumpOn = false;
+static bool canPump = true;
+
+static const int buz_pin = 4;
+
+static const int pump_pin = 10;
+static const int pump_pwm_val = 200;
+static const int pump_dir_pin = 12;
 
 void setup()
 {
 	Serial.begin(9600);
+	pinMode(pump_pin, OUTPUT);
+	pinMode(pump_dir_pin, OUTPUT);
+	pinMode(buz_pin, OUTPUT);
+	pinMode(SOIL_PIN, INPUT);
+
+	digitalWrite(pump_dir_pin, HIGH);
 }
 
 void loop()
 {
+	delay(500);
+
 	int waterValue = analogRead(SOIL_PIN);
 	Serial.println(waterValue);
 
-	if (waterValue > AVG)
+	if (!canPump)
+	{
+		return;
+	}
+
+	if (waterValue >= AVG)
 	{
 		// Turn on the pump
+		if (!pumpOn)
+		{
+			turnPumpOn();
+		}
+		else if (millis() - pumpStartMilis > MAX_PUMP_TIME)
+		{
+			turnPumpOff();
+			canPump = false;
 
-		turnPumpOn();
+			// for some reason the pump is not turning off
+			// need device reset
+			digitalWrite(buz_pin, HIGH);
+		}
 	}
 	else
 	{
 		// Turn off the pump
-
-		turnPumpOff();
+		if (pumpOn && millis() - pumpStartMilis > MIN_PUMP_TIME)
+		{
+			turnPumpOff();
+		}
 	}
-
-	delay(500);
 }
 
 void turnPumpOff()
 {
 	Serial.println("Pump off");
 	pumpOn = false;
+	analogWrite(pump_pin, 0);
+	pumpStartMilis = 0;
 }
 
 void turnPumpOn()
 {
 	Serial.println("Pump on");
 	pumpOn = true;
+	analogWrite(pump_pin, pump_pwm_val);
+	pumpStartMilis = millis();
 }
